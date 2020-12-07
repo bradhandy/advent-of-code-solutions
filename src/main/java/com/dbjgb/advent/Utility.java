0cba1fd8f91dfaaa3b9d821889ef60e1ec3aed7a
bhandy <brad.handy@gmail.com>
2020-12-07 10:32:46 -0500

    Solution for Advent of Code 2015 problem twelve. - Added the Jackson library - Added a new utility method.

--- src/main/java/com/dbjgb/advent/Utility.java
+++ src/main/java/com/dbjgb/advent/Utility.java
@@ -1,16 +1,21 @@
 package com.dbjgb.advent;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public final class Utility {
 
   public static BufferedReader openInputFile(String path) {
+    return new BufferedReader(new InputStreamReader(openInputStream(path)));
+  }
+
+  public static InputStream openInputStream(String path) {
     String fullPath = "com/dbjgb/advent/twenty/" + path;
     InputStream input =
         Thread.currentThread().getContextClassLoader().getResourceAsStream(fullPath);
-    return new BufferedReader(new InputStreamReader(input));
+    return input;
   }
 
   private Utility() {}
