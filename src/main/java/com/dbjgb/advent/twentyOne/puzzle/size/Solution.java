7934d3be304cd170b53f1d790e127d79c05ab8b3
bhandy <bhandy@vertafore.com>
2021-12-06 11:10:52 -0500

    Implemented Day 6 of 2021.

---
+++ src/main/java/com/dbjgb/advent/twentyOne/puzzle/size/Solution.java
@@ -0,0 +1,46 @@
+package com.dbjgb.advent.twentyOne.puzzle.size;
+
+import com.dbjgb.advent.Utility;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+  public static void main(String... args) throws Exception {
+    printTotalLanternFishAfterDays(80);
+    printTotalLanternFishAfterDays(256);
+  }
+
+  private static void printTotalLanternFishAfterDays(int numberOfDays) throws IOException {
+    try (BufferedReader inputReader = Utility.openInputFile("twentyOne/puzzle/six/input.txt")) {
+      String delimitedAges = inputReader.readLine();
+      List<Integer> initialLanternFishAges =
+          Arrays.stream(delimitedAges.split(","))
+              .map(Integer::valueOf)
+              .collect(Collectors.toList());
+
+      long[] newFishPerDay = new long[numberOfDays];
+      Arrays.fill(newFishPerDay, 0);
+
+      for (int lanternFishAge : initialLanternFishAges) {
+        for (int day = lanternFishAge; day <= (numberOfDays - 1); day += 7) {
+          newFishPerDay[day]++;
+        }
+      }
+
+      for (int day = 0; day <= (numberOfDays - 1); day++) {
+        for (int newFishBucket = day + 9; newFishBucket <= (numberOfDays - 1); newFishBucket += 7) {
+          newFishPerDay[newFishBucket] += newFishPerDay[day];
+        }
+      }
+
+      System.out.printf(
+          "Total new fish: %d\n",
+          Arrays.stream(newFishPerDay).reduce(0, Long::sum) + initialLanternFishAges.size());
+    }
+  }
+}
